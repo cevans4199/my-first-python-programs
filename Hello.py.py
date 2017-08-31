f223b7aed1d10725c3d49803b91b18e738f90b59
cevans4199 <31477383+cevans4199@users.noreply.github.com>
2017-08-31 09:27:43 -0400

    Delete Hello.py.py

--- Hello.py.py
+++
@@ -1,9 +0,0 @@
-# This programsays hello and greets a person by name.
-#
-# Your Name
-# August 24, 2017
-
-print ("Hello.")
-print ("What is your name?")
-name = input ()
-print (" It is good to meet you, " + name + ".")
